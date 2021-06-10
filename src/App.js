cc9d1ea4a4bdddf997148a084cc40070acd1d1f2
Felix <felix-mayer@gmx.de>
2021-06-10 16:29:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Link } from '@reach/router';
 import './App.css';
 
-function App() {
+import HomePage from './components/HomePage';
+import People from './components/People';
+import Planets from './components/Planets';
+import Starship from './components/Starship';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link style={{ textDecorationLine: 'none', color: 'black' }} to="/" >HomePage</Link>
+      <br /><hr />
+      <Router className='App'>
+        <HomePage path="/" />
+        <People path="/people/:id" />
+        <Planets path="/planets/:id" />
+        <Starship path="/starships/:id" />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
